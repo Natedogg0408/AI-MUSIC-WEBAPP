777684d0824482a6a86b88a1e6446f34a423a8c8
Natedogg0408 <77352991+Natedogg0408@users.noreply.github.com>
2021-09-05 11:21:37 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 song_1 = "";
 song_2 = "";
-
+leftWristY = 0;
+leftWristX = 0;
+rightWristX = 0;
+rightWristY = 0;
 function setup(){
     canvas = createCanvas(600, 500);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw(){
@@ -18,4 +24,23 @@ function draw(){
 function preload(){
  song_1 = loadSound('music.mp3');
  song_2 = loadSound('music2.mp3');
+}
+
+
+function modelLoaded(){
+    console.log("PoseNet is initialized!");
+}
+
+
+function gotPoses(results){
+    if(results.length > 0 ){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWrist X = "+ leftWristX + "leftWrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWrist X = "+ rightWristX + "rightWrist Y = " + rightWristY);
+    }
 }
